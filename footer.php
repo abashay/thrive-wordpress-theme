369a3e7a05afeda451b0502167cd664f45c22503
Doc <hi@jamesdoc.com>
2015-10-17 15:37:13 +0100

    Tidy up footer (Search and Email)

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
 			<form class="footer__thrive__search" role="search" method="get" action="<?php echo get_site_url(); ?>">
 				<label for="txt_search">
 					<span class="screen-reader-text">Search for:</span>
-					<input type="search"  title="Search for:" placeholder="Tell me what you are looking for" id="txt_search" class="search-field" name="s" />
+					<input type="text"  title="Search for:" id="txt_search" class="search-field" name="s" />
 				</label>
 				<input type="submit" class="btn search-submit" value="Search" />
 			</form>
@@ -34,21 +34,21 @@
 
 			<form action="//innovista.us6.list-manage.com/subscribe/post?u=4c7a819d6edce6c9c111f6e2d&amp;id=8baf4fdf25" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="footer__thrive__signup validate" target="_blank" novalidate>
 
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>
 
 				<div class="mc-field-group">
-					<label for="mce-EMAIL">Join our mailing list</label>
-					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Your main email address">
+					<label for="mce-EMAIL">Join our mailing list:</label>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your@emailaddress.com">
 				</div>
 
 			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_4c7a819d6edce6c9c111f6e2d_8baf4fdf25" tabindex="-1" value=""></div>
 			    <input type="hidden" value="2" name="group[6109][2]" id="mce-group[6109]-6109-1">
-			    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn search-submit">
 
-			    <small>We will never pass your details to anyone else. Promise.</small>
+			    <small>We will never pass your details to anyone else.</small>
 			</form>
 
 		</section>
