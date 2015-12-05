0be17275a9e221301c4ed23323c4ccdd62f6c3df
Doc <hi@jamesdoc.com>
2015-12-05 19:40:29 +0000

    Tidy up the 404 page slightly

--- 404.php
+++ 404.php
@@ -9,21 +9,36 @@
 
 get_header(); ?>
 
+	<section class="banner">
+
+		<a href="/">
+
+			<div class="banner__hero">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/banner.jpg" alt="" />
+			</div>
+
+			<div class="banner__overlay">
+
+				<div class="banner__overlay__text">
+
+					<h1>4 OH 4</h1>
+
+					<p>Oh no! This page cannot be found.</p>
+
+				</div>
+
+			</div>
+
+		</a>
+
+	</section>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+			<section class="post__content main_content">
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
+				<?php get_search_form(); ?>
+			</section><!-- .page-content -->
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
