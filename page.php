58d393353264a2a7f669bd8cc8c467202074e41a
Doc <hi@jamesdoc.com>
2015-12-14 21:56:13 +0000

    Add sidebar to join page
    + tidy up the file a little bit!

--- page.php
+++ page.php
@@ -1,63 +1,35 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
+
+// Set up page banner
+$image = wp_get_attachment_image_src(
+    get_post_thumbnail_id(),
+    'large',
+    false);
+
+$banner = (object) array(
+    'title' => get_the_title(),
+    'subtitle' => get_post_meta( get_the_ID(), 'banner-text', true ),
+    'image' => $image[0]
+);
+
+$subpages = array(
+    'parent' => wp_get_post_parent_id( get_the_ID() ),
+    'cover' => false,
+    'width'=>'33'
+);
 
 get_header();
 
 // Start the loop.
-while ( have_posts() ) : the_post(); ?>
+while ( have_posts() ) : the_post();
 
-<section class="banner banner--post banner--400">
+    // Output page banner
+    include_once('templates/tpl_page_banner.php');
 
-    <div class="banner__main">
-
-        <div class="banner__hero">
-            <?php
-                $thumb_id = get_post_thumbnail_id();
-                if (isset($thumb_id) && $thumb_id != "") {
-                    $bannerimage = wp_get_attachment_image_src($thumb_id, 'large', false);
-                    $bannerimage = $bannerimage[0];
-                } else {
-                    // Should look up a default in the settings
-                    $bannerimage = get_template_directory_uri() . '/assets/young-achievers-dinner-2013.jpg';
-                }
-            ?>
-            <img src="<?php echo $bannerimage; ?>" alt="" />
-        </div>
-
-        <div class="banner__overlay">
-
-            <div class="banner__overlay__text">
-
-                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-                <?php
-                    $banner_intro = get_post_meta( get_the_ID(), 'banner-text', true );
-                    // check if the custom field has a value
-                    if( ! empty( $banner_intro ) ) {
-                      echo sprintf('<p>%s</p>', $banner_intro);
-                    }
-                ?>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
+?>
 
 
-<?php if ( preg_match( '#^teams/.+/.+?$#', $wp->request ) OR preg_match( '#^about/.+/?$#', $wp->request ) ) : ?>
+<?php if ( preg_match( '#^teams/.+/.+?$#', $wp->request ) OR preg_match( '#^about/.+/?$#', $wp->request ) OR preg_match( '#^join/.+/?$#', $wp->request ) ) : ?>
 
     <section class="post__content g_main_content">
 
@@ -67,21 +39,13 @@ while ( have_posts() ) : the_post(); ?>
 
     <section class="g_sidebar">
 
-
-        <?php
-            $atts = array(
-                'parent' => wp_get_post_parent_id( get_the_ID() ),
-                'cover' => false,
-                'width'=>'33'
-            );
-            echo thrive_get_subpages($atts);
-        ?>
+        <?php echo thrive_get_subpages($subpages); ?>
 
     </section>
 
 <? else: ?>
 
-    <section class="post__content">
+    <section class="post__content g_main_content g_main_content--lonely">
 
         <?php the_content(); ?>
 
